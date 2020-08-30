4c67ba97e113c67c77349be0714c665e7cc0a0db
Paolo Marcolini <canalla88.13@gmail.com>
2020-08-30 19:24:37 -0300

    Issue-2. Created Category module

--- src/app/categories/category-form/category-form.component.ts
+++ src/app/categories/category-form/category-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormLayout } from 'src/app/shared/models/form-layout.model';
+import { Category } from '../models/category.model';
+import { Router, ActivatedRoute } from '@angular/router';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-category-form',
@@ -6,10 +11,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category-form.component.css']
 })
 export class CategoryFormComponent implements OnInit {
+  categoryForm: FormGroup;
+  formInfo: FormLayout;
 
-  constructor() { }
+  isEditing: boolean;
+  category: Category = new Category();
+
+  get name() { return this.categoryForm.get('name'); }
+
+  constructor(
+    private fb: FormBuilder,
+    private categoryService: CategoryService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.categoryForm = this.modelCreate();
+
+    this.isEditing = this.route.snapshot.url.toString().includes('edit');
+
+    if (this.isEditing) {
+      this.formInfo = {
+        submitText: 'Update',
+        title: 'Category',
+        subtitle: 'Edit category',
+        isEditing: true
+      }
+
+      this.category = this.route.snapshot.data.category;
+      this.name.patchValue(this.category.name);
+    }
+    else {
+      this.formInfo = {
+        submitText: 'Save',
+        title: 'Category',
+        subtitle: 'Create category',
+        isEditing: false
+      }
+    }
   }
 
+  modelCreate = () => this.fb.group({
+    name: ['', Validators.required]
+  })
+
+  onSubmit = () => {
+    if (!this.categoryForm.valid) return;
+    const categoryModified = new Category();
+    categoryModified.id = this.category.id;
+    categoryModified.name = this.name.value;
+
+    this.isEditing
+      ? this.categoryService.update(categoryModified)
+        .subscribe(this.goToList)
+      : this.categoryService.add(categoryModified)
+        .subscribe(this.goToList);
+  }
+
+  goToList = () => this.router.navigate(['categories']);
 }
